fe820eb703defcc3376a8d430f732db7d0f529b0
Thomas Roulin <thomasroulinch@gmail.com>
2018-04-09 11:32:04 +0200

    hard links, dammit keyboard

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,9 @@
-/home/megaturbo/.config/fish/config.fish
+set fish_greeting ""
+
+# git things
+alias gs='git status'
+alias gd='git diff'
+
+function yaour
+    yaourt --color -Ss $argv | grep '^\S*[ce]\w*/' -A1
+end
