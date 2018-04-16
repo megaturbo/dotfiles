f418193319396f8ecb8d4bbd13a25848b7d64d22
Thomas Roulin <thomasroulinch@gmail.com>
2018-04-16 14:56:14 +0200

    Add screen scripts and update i3 colors

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,19 @@
 set fish_greeting ""
 
-# git things
+# Aliases
 alias gs='git status'
 alias gd='git diff'
 
+# should be in functions folder
 function yaour
     yaourt --color -Ss $argv | grep '^\S*[ce]\w*/' -A1
 end
 
+# Globals
 set PATH ~/.npm-global/bin $PATH
 set EDITOR vim
+
+# Startup routines
+#  pywal
+#cat ~/.cache/wal/sequences
+#source ~/.cache/wal/colors-tty.sh
