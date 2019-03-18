bef85c92499650eb64140af16c99fd77fe89a938
megaturbo <megaturbo@truelevel.io>
2019-03-18 11:22:38 +0100

    Clean up .zshrc add solc_setver

---
+++ zsh/custom/path.zsh
@@ -0,0 +1,17 @@
+## path config
+
+# python
+export PATH=/home/megaturbo/.local/bin:$PATH
+# yarn global
+export PATH=$(yarn global bin):$PATH
+# npm global
+export PATH=~/.npm-global/bin:$PATH
+# cargo
+export PATH=~/.cargo/bin:$PATH
+# android
+export ANDROID_HOME=$HOME/Android/Sdk
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+# ruby
+export PATH=$PATH:$HOME/.gem/ruby/2.5.0/bin
