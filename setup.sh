3250dfdc6b7e6be5d1f0d4a87ca76cb0ff1124ff
megaturbo <megaturbo@truelevel.io>
2019-06-27 12:33:59 +0200

    work on setup script, litteraly nothing

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # RUN THIS FILE FROM THE ROOT OF THIS FOLDER
 
+# create symbolic links
 ln -fs ~/
+
+# wallpapers Pictures
+cp -rs .wallpapers/ ~/.wallpapers/
 #cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+# install packages
